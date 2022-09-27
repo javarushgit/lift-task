4d95f68e07f472e792e16d1117aef23b0318c025
javarushgit <110184764+javarushgit@users.noreply.github.com>
2022-09-27 19:36:26 +0300

    Update Floor.java

--- src/main/java/com/taslitsky/component/Floor.java
+++ src/main/java/com/taslitsky/component/Floor.java
@@ -12,10 +12,8 @@ public class Floor {
   private final int floorId;
 
   public Floor(Building building, int floorId) {
-    System.out.println("hi");
     this.building = building;
     this.floorId = ++floorId;
-    System.out.println("1");
     generateQueue();
   }
 
@@ -33,18 +31,13 @@ public class Floor {
   private void generateQueue() {
     Random random = new Random();
     FloorPropertiesReader propertiesReader = new FloorPropertiesReader();
-    System.out.println("2");
     int floorMinQueue = propertiesReader.getMinQueue();
     int floorMaxQueue = propertiesReader.getMaxQueue();
-    System.out.println("3");
     int queueCount = floorMinQueue + random.nextInt(floorMaxQueue-floorMinQueue + 1);
-    System.out.println("4");
     queue = new ArrayList<>(queueCount);
 
     for (int i = 0; i < queueCount; i++) {
-      System.out.println("4,5");
       queue.add(new Person(this));
     }
-    System.out.println("5");
   }
 }
